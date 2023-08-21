b45e2b5e2b9083db663e49caec3e08d23e2b59c4
Ariful Islam <89648812+Arif111866@users.noreply.github.com>
2023-08-21 14:24:03 +0600

    Update main.dart
    new apple

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class CalculatorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator App',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      // theme: ThemeData(primarySwatch: Colors.black87),
       home: CalculatorScreen(),
     );
   }
@@ -19,28 +19,30 @@ class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My First Calculator')),
+      backgroundColor: Color(0xFF212121), // Dark background color
+      appBar: AppBar(title: Text('My iPhone Calculator')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text(
-              'Input field', // Display the ongoing calculation or result here
-              style: TextStyle(fontSize: 24),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+              child: Text(
+                'Inputn  // Display the ongoing calculation or result here
+                style: TextStyle(fontSize: 48, color: Colors.white),
+              ),
             ),
             SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              ),
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ,),
-              textAlign: TextAlign.end,
-              readOnly: true,
-              controller: TextEditingController(text: 'give me input'), // Replace '579' with your current input
-            )
-            ,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalculatorButton(text: 'C'),
+                CalculatorButton(text: 'M-'),
+                CalculatorButton(text: '%'),
+                CalculatorButton(text: '÷')
+              ],
+            ),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -48,7 +50,7 @@ class CalculatorScreen extends StatelessWidget {
                 CalculatorButton(text: '7'),
                 CalculatorButton(text: '8'),
                 CalculatorButton(text: '9'),
-                CalculatorButton(text: '÷'),
+                CalculatorButton(text: '*'),
               ],
             ),
             SizedBox(height: 10),
@@ -58,7 +60,7 @@ class CalculatorScreen extends StatelessWidget {
                 CalculatorButton(text: '4'),
                 CalculatorButton(text: '5'),
                 CalculatorButton(text: '6'),
-                CalculatorButton(text: '×'),
+                CalculatorButton(text: '-'),
               ],
             ),
             SizedBox(height: 10),
@@ -68,7 +70,7 @@ class CalculatorScreen extends StatelessWidget {
                 CalculatorButton(text: '1'),
                 CalculatorButton(text: '2'),
                 CalculatorButton(text: '3'),
-                CalculatorButton(text: '-'),
+                CalculatorButton(text: '+'),
               ],
             ),
             SizedBox(height: 10),
@@ -78,7 +80,7 @@ class CalculatorScreen extends StatelessWidget {
                 CalculatorButton(text: '0'),
                 CalculatorButton(text: '.'),
                 CalculatorButton(text: '='),
-                CalculatorButton(text: '+'),
+                CalculatorButton(text: 'AC')
               ],
             ),
           ],
@@ -91,7 +93,8 @@ class CalculatorScreen extends StatelessWidget {
 class CalculatorButton extends StatelessWidget {
   final String text;
 
-  const CalculatorButton({required this.text});
+
+  const CalculatorButton({required this.text,});
 
   @override
   Widget build(BuildContext context) {
@@ -99,10 +102,15 @@ class CalculatorButton extends StatelessWidget {
       width: 80,
       height: 80,
       child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.white30 , // Dark gray button color
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(24),
+        ),
         onPressed: () {},
         child: Text(
           text,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 33, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
     );
